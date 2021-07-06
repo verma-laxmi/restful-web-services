4453eb07c1a8b01094609d25c1ee07b56f2af1ba
verma-laxmi <chikki@Chikkis-MacBook-Pro.local>
2021-07-06 18:31:31 +0530

    adding resources

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into TODO
+(id, description, is_done, target_date, username)
+values (1, 'Learn to dance', TRUE, now(), 'fondofit');
+insert into TODO
+(id, description, is_done, target_date, username)
+values (2, 'Learn about microservicces 2', TRUE , now(), 'fondofit');
+insert into TODO
+(id, description, is_done, target_date, username)
+values (3, 'Learn about angular', TRUE , now(), 'fondofit');

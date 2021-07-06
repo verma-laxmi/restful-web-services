4f98c9ad7f7c844709e9dc0219062f98ca9c0cd7
verma-laxmi <chikki@Chikkis-MacBook-Pro.local>
2021-07-06 18:42:39 +0530

    adding test cases

---
+++ src/test/java/com/fondofit/rest/webservices/restfulwebservices/TestExample.java
@@ -0,0 +1,13 @@
+package com.fondofit.rest.webservices.restfulwebservices;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestExample {
+
+	@Test
+	public void testSample() {
+		assertEquals("test", "test");
+	}
+}

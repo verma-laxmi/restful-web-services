c42630e6776391672b8ca77c63e812b1d67221d4
verma-laxmi <chikki@Chikkis-MacBook-Pro.local>
2021-07-10 10:43:15 +0530

    test for code covarge

--- src/test/java/com/fondofit/rest/webservices/restfulwebservices/TestExample.java
+++ src/test/java/com/fondofit/rest/webservices/restfulwebservices/TestExample.java
@@ -2,12 +2,17 @@ package com.fondofit.rest.webservices.restfulwebservices;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
+
+import com.fondofit.rest.webservices.restfulwebservices.todo.Todo;
 
 public class TestExample {
 
 	@Test
 	public void testSample() {
+		Todo todo = new Todo(1, "username", "desc", new Date(), false);
 		assertEquals("test", "test");
 	}
 }
